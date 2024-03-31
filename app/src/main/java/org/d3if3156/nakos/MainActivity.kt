5d8e856c5a144610c1e232f82efe9684632ffc51
appratamaa <andreputrap30@gmail.com>
2024-03-31 21:38:12 +0700

    Top App 2

--- app/src/main/java/org/d3if3156/nakos/MainActivity.kt
+++ app/src/main/java/org/d3if3156/nakos/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
@@ -14,9 +16,11 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import org.d3if3156.nakos.ui.theme.NAKOSTheme
 
 class MainActivity : ComponentActivity() {
@@ -54,6 +58,23 @@ fun MainScreen(content: @Composable (Modifier) -> Unit) {
 
     }
     }
+@Composable
+fun Counter() {
+    MainScreen {
+        modifier ->
+        Column (
+            modifier = modifier
+                .fillMaxSize()
+                .padding(16.dp),
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(text = "0",
+                style = MaterialTheme.typography.displayLarge
+            )
+    }
+    }
+}
 @Composable
 fun Greeting(name: String) {
     MainScreen { modifier ->
